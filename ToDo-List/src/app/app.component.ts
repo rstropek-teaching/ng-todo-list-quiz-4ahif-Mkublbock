4c1c00e26bc79144fd434ddc896975584dfb1ebc
Matthias Küblböck <matthias.kueblboeck@yahoo.de>
2018-03-02 16:59:33 +0100

    table with todos

--- ToDo-List/src/app/app.component.ts
+++ ToDo-List/src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+interface IPerson {
+  name: string;
+}
+
+interface ITodo {
+  id: number;
+  description: string;
+  assignedTo: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public people: Observable<IPerson[]>;
+  public todos: Observable<ITodo[]>;
+
+  displayedColumns = ['id', 'description', 'assignedTo'];
+
+  constructor(private httpClient: HttpClient) {
+    this.people = httpClient.get<IPerson[]>('http://localhost:8080/api/people');
+    this.todos = httpClient.get<ITodo[]>('http://localhost:8080/api/todos');
+  }
 }
